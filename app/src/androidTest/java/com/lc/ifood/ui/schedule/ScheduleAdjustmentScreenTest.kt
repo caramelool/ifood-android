a9cfd087e95af1078ad480841ee2cdfe5034875b
Lucas Caramelo <lucascaramelo@gmail.com>
2026-04-28 01:50:04 -0300

    optimize imports

--- app/src/androidTest/java/com/lc/ifood/ui/schedule/ScheduleAdjustmentScreenTest.kt
+++ app/src/androidTest/java/com/lc/ifood/ui/schedule/ScheduleAdjustmentScreenTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onFirst
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/com/lc/ifood/domain/usecase/UpdateMealScheduleUseCase.kt
+++ app/src/main/java/com/lc/ifood/domain/usecase/UpdateMealScheduleUseCase.kt
@@ -2,7 +2,6 @@ package com.lc.ifood.domain.usecase
 
 import com.lc.ifood.domain.model.MealSchedule
 import com.lc.ifood.domain.repository.MealScheduleRepository
-import com.lc.ifood.worker.MealRecommendationScheduler
 import javax.inject.Inject
 
 /**
